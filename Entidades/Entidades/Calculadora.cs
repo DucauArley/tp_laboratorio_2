2e076fbe227fe10070c9830d5c7b58cf61ca7790
DucauArley <arleyducau@gmail.com>
2018-09-12 14:04:54 -0300

    Tp

---
+++ Entidades/Entidades/Calculadora.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Calculadora
+    {
+        private static string ValdiarOperador(string operador)
+        {
+            string retorno = "+";
+
+            if(operador == "-" || operador == "/" || operador == "*" || operador == "+")
+            {
+                retorno = operador;
+            }
+
+            return retorno;
+        }
+
+        public double Operar(Numero n1, Numero n2, string operador)
+        {
+            double resultado;
+
+            operador = ValdiarOperador(operador);
+
+            switch (operador)
+            {
+
+
+
+
+            }
+
+
+
+            return resultado;
+        }
+
+
+
+    }
+}
---
+++ Entidades/Entidades/Numero.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        private double numero;
+
+        public Numero()
+        {
+
+        }
+
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string strNumero)
+        {
+
+        }
+
+        private double ValidarNumero(string strNumero)
+        {
+            double numero = 0;
+
+            double.TryParse(strNumero, out numero);
+
+            return numero;
+        }
+
+        
+        public static double operator +(Numero n1, Numero n2)
+        {
+            double num;
+
+            num = n1.numero + n2.numero;
+
+            return num;
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            double num;
+
+            num = n1.numero - n2.numero;
+
+            return num;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            double num;
+
+            num = n1.numero / n2.numero;
+
+            return num;
+        }
+
+        public static double operator *(Numero n1, Numero n2)
+        {
+            double num;
+
+            num = n1.numero + n2.numero;
+
+            return num;
+        }
+
+
+    }
+}
