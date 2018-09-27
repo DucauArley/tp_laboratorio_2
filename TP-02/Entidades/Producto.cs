770043376656b0e340656c4d066922a65ede4eb2
DucauArley <arleyducau@gmail.com>
2018-09-27 15:05:47 -0300

    Tp

--- TP-02/Entidades/Producto.cs
+++ TP-02/Entidades/Producto.cs
@@ -9,7 +9,7 @@ namespace Entidades_2018
     /// <summary>
     /// La clase Producto no deberá permitir que se instancien elementos de este tipo.
     /// </summary>
-    public sealed class Producto
+    public abstract class Producto
     {
         enum EMarca
         {
@@ -22,7 +22,7 @@ namespace Entidades_2018
         /// <summary>
         /// ReadOnly: Retornará la cantidad de ruedas del vehículo
         /// </summary>
-        abstract short CantidadCalorias { get; set; }
+        protected abstract short CantidadCalorias { get; set; }
 
         /// <summary>
         /// Publica todos los datos del Producto.
@@ -33,16 +33,16 @@ namespace Entidades_2018
             return this;
         }
 
-        private static explicit operator string(Producto p)
+        public static explicit operator string(Producto p)
         {
             StringBuilder sb = new StringBuilder();
 
-            sb.AppendLine("CODIGO DE BARRAS: {0}\r\n", p.codigoDeBarras);
-            sb.AppendLine("MARCA          : {0}\r\n", p.marca.ToString());
-            sb.AppendLine("COLOR EMPAQUE  : {0}\r\n", p.colorPrimarioEmpaque.ToString());
+            sb.AppendLine("CODIGO DE BARRAS: {0}\r\n" + p.codigoDeBarras);
+            sb.AppendLine("MARCA          : {0}\r\n" + p.marca.ToString());
+            sb.AppendLine("COLOR EMPAQUE  : {0}\r\n" + p.colorPrimarioEmpaque.ToString());
             sb.AppendLine("---------------------");
 
-            return sb;
+            return sb.ToString();
         }
 
         /// <summary>
